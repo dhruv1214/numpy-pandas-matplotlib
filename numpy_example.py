e4e1d2b967bb302353c9b36017b151d11345d59d
aditya desle <50102208+adityadesle@users.noreply.github.com>
2020-06-11 11:32:42 +0530

    Add files via upload

---
+++ numpy_example.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 10 12:01:46 2020
+
+@author: 66IN
+"""
+
+
+import numpy as np
+import sys
+
+'''
+a = np.array([(1,2,3),(4,5,6)])
+
+print((a)) 
+
+
+
+
+s = range(1000)
+print(sys.getsizeof(5)*len(s))
+
+d = np.arange(1000)
+print(d.size*d.itemsize)
+
+'''
+
---
+++ pandas_example.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 11 09:38:15 2020
+
+@author: 66IN
+"""
+
+
+import pandas as pd
+
+import numpy as np
+
+
+s = pd.Series([1,2,3,4,np.nan,5,6])   #using series in pandas
+
+
+print(s)
+
+# example of dataframe :
+
+
+d = pd.date_range('20200301',periods=10)
+
+print(d)
+
+df = pd.DataFrame(np.random.randn(10,4),index=d,columns=['A','B','C','D'])
+
+print(df)
